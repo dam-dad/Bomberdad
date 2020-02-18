80feb71902ba7c88bf0ade77e87cea7751ff7e47
Pablogg98 <55440053+Pablogg98@users.noreply.github.com>
2020-02-18 09:18:14 +0000

    Cambio New Menu

---
+++ src/main/java/dad/javafx/bomberdad/BombermanAppUIController.java
@@ -0,0 +1,19 @@
+package dad.javafx.bomberdad;
+
+import com.almasb.fxgl.ui.UIController;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+public class BombermanAppUIController implements UIController {
+
+	@FXML
+	private VBox vbox;
+	
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
